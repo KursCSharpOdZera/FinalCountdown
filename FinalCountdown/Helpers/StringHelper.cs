cbe9c5eb7fb26839b74a27118d3ae3dbe83e545e
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-29 22:31:48 +0100

    Added PadLine() method

--- FinalCountdown/Helpers/StringHelper.cs
+++ FinalCountdown/Helpers/StringHelper.cs
@@ -1,8 +1,13 @@
-﻿namespace FinalCountdown.Helpers
+﻿using System;
+
+namespace FinalCountdown.Helpers
 {
     public static class StringHelper
     {
         public static string PadNumber(int number) =>
             number.ToString().PadLeft(2);
+
+        public static string PadLine(string line) =>
+            line.PadRight(Console.LargestWindowWidth);
     }
 }
