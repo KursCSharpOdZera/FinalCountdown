ed94486699ce23e3c6e1b2eed576547dfe42aef8
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-29 22:09:01 +0100

    Added WriteLabel() method

---
+++ FinalCountdown/ClockView.cs
@@ -0,0 +1,32 @@
+﻿using FinalCountdown.Dictionaries;
+using FinalCountdown.Enums;
+using FinalCountdown.Helpers;
+using System;
+
+namespace FinalCountdown
+{
+    public class ClockView
+    {
+        FormLabelDictionary _formLabels = FormLabelDictionary.Instance;
+
+        public ClockView()
+        {
+            ConsoleHelper.PrepareConsole();
+            PrepareRawForm();
+        }
+
+        private void PrepareRawForm()
+        {
+            WriteLabel(FormLabelName.ProgramHeader);
+        }
+
+        private void WriteLabel(FormLabelName formLabelName)
+        {
+            ConsoleHelper.WriteAtCursorPosition
+                (
+                    text:           _formLabels[formLabelName].Literal,
+                    cursorPosition: _formLabels[formLabelName].CursorPosition
+                );
+        }
+    }
+}
