b3b2f4fde6453bbf01e5b5b75983defe76ecd2bd
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-29 22:50:10 +0100

    Added ProgramLoop class

---
+++ FinalCountdown/ProgramLoop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace FinalCountdown
+{
+    public class ProgramLoop
+    {
+        private const int OneSecond = 1000;
+
+        private readonly ClockView _clockView;
+        private bool _programLoopStop = false;
+
+        public ProgramLoop(ClockView clockView)
+        {
+            _clockView = clockView;
+        }
+
+        public void Run()
+        {
+            do
+            {
+                _clockView.Update();
+                CaptureEscButton();
+                Thread.Sleep(OneSecond);
+            } while (_programLoopStop == false);
+        }
+
+        private void CaptureEscButton()
+        {
+            if (Console.KeyAvailable)
+            {
+                _programLoopStop = Console
+                    .ReadKey(intercept: true)
+                    .Key == ConsoleKey.Escape;
+            }
+        }
+    }
+}
