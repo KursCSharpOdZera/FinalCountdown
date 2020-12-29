59ee5ac1778fdc016a9faaad2d20efc4cfc67811
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-29 21:53:37 +0100

    Added ConsoleHelper

---
+++ FinalCountdown/Helpers/ConsoleHelper.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace FinalCountdown.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void PrepareConsole()
+        {
+            Clear();
+            CursorVisible = false;
+        }
+    }
+}
